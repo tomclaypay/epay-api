c3fb3cabaecf3998b219741982fa597d6d340611
Tom <tomclaypay@gmail.com>
2025-07-04 02:39:45 +0700

    add webhook upay

--- src/modules/aggregators/webhooks/upay-webhooks/upay-webhooks.controller.ts
+++ src/modules/aggregators/webhooks/upay-webhooks/upay-webhooks.controller.ts
@@ -1,9 +1,23 @@
+import { UpayWebhookDto } from '@/modules/aggregators/webhooks/upay-webhooks/dto/request.dto'
+import { UpayWebhooksService } from '@/modules/aggregators/webhooks/upay-webhooks/upay-webhooks.service'
 import { VirtualWebhooksService } from '@/modules/aggregators/webhooks/virtual-webhooks/virtual-webhooks.service'
-import { Controller } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 
 @ApiTags('Webhook')
 @Controller()
 export class UpayWebhooksController {
-  constructor(private virtualWebhooksService: VirtualWebhooksService) {}
+  constructor(private upayWebHookService: UpayWebhooksService) {}
+
+  @UsePipes(new ValidationPipe({ transform: true }))
+  @Post('webhooks/upay')
+  async handleUpayWebhook(@Body() upayWebHookDto: UpayWebhookDto) {
+    return this.upayWebHookService.handleUpayWebhook(upayWebHookDto)
+  }
 }
